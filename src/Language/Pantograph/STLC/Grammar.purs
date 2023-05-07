14822e59ae2b54f7a8f58efb6ce2c5431b667e69
Jacob Prinz <jacobeliasprinz@gmail.com>
2023-05-07 14:22:43 -0400

    Hole rule

--- src/Language/Pantograph/STLC/Grammar.purs
+++ src/Language/Pantograph/STLC/Grammar.purs
@@ -107,7 +107,13 @@ rules = [
         [exp SVar [var g, var a]]
         (exp SVar [exp CCons [var g, var b], var a])
 
-    --------------------------- Metadata -------------------------------------------
+    --------------------------- Other ----------------------------------------------
+    ,
+    -- hole
+    let s = freshMetaVar unit in
+    G.Rule (Set.fromFoldable [s])
+        []
+        (var s)
     ,
     -- newline
     let s = freshMetaVar unit in
