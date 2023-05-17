c75e2da368f9feca713c9ba66eb36d6947c68986
Jacob Prinz <jacobeliasprinz@gmail.com>
2023-05-17 14:27:02 -0400

    Fixed up STLC rules

--- src/Language/Pantograph/STLC/Grammar.purs
+++ src/Language/Pantograph/STLC/Grammar.purs
@@ -58,7 +58,7 @@ instance Expr.IsExprLabel ExprLabel where
 type Expr = Expr.Expr ExprLabel
 type MetaExpr = Expr.MetaExpr ExprLabel
 
-data RuleLabel = Lam | App | Z | S | Var | Let | Base -- | TermBind
+data RuleLabel = Lam | App | Z | S | Var | Let | Base | Arrow | If | Newline -- | TermBind
 
 derive instance Generic RuleLabel _
 derive instance Eq RuleLabel
@@ -73,6 +73,9 @@ instance Pretty RuleLabel where
     pretty Var = "var"
     pretty Let = "let"
     pretty Base = "base"
+    pretty Arrow = "->"
+    pretty If = "if"
+    pretty Newline = "newline"
 
 instance Enum RuleLabel where
     succ x = genericSucc x
@@ -92,6 +95,7 @@ instance IsRuleLabel ExprLabel RuleLabel where
     prettyExprF'_unsafe_RuleLabel (Base /\ []) = "Base"
 
     language = TotalMap.makeTotalMap case _ of
+        -- Terms
         App -> G.makeRule ["gamma", "alpha", "beta"] \[gamma, alpha, beta] ->
             [ gamma ⊢ exp TArrow [alpha, beta]
             , gamma ⊢ alpha ]
@@ -102,14 +106,6 @@ instance IsRuleLabel ExprLabel RuleLabel where
             , exp CCons [gamma, name, alpha] ⊢ beta ]
             /\ --------
             ( gamma ⊢ exp TArrow [alpha, beta] )
-        Z -> G.makeRule ["gamma", "name", "alpha"] \[gamma, name, alpha] ->
-            [ ]
-            /\ --------
-            ( exp CCons [gamma, name, alpha] ⊢⊢ alpha )
-        S -> G.makeRule ["gamma", "alpha", "name", "beta"] \[gamma, alpha, name, beta] ->
-            [ gamma ⊢⊢ alpha ]
-            /\ --------
-            ( exp CCons [gamma, name, beta] ⊢⊢ alpha )
         Var -> G.makeRule ["gamma", "x"] \[gamma, alpha] ->
             [ gamma ⊢⊢ alpha ]
             /\ --------
@@ -120,10 +116,33 @@ instance IsRuleLabel ExprLabel RuleLabel where
             , exp CCons [gamma, name, alpha] ⊢ beta ]
             /\ --------
             ( gamma ⊢ beta )
+        If -> G.makeRule ["a", "g"] \[a, g] ->
+            []
+            /\ --------
+            (g ⊢  exp TArrow [exp TBool [], exp TArrow [a, exp TArrow [a, a]]])
+        -- Types
+        Arrow -> G.makeRule ["a", "b"] \[a, b] ->
+            [exp SType [a], exp SType [b]]
+            /\ --------
+            (exp SType [exp TArrow [a, b]])
         Base -> G.makeRule [] \[] ->
             []
             /\ --------
             ( exp SType [exp TBase []] )
+        -- Variables
+        Z -> G.makeRule ["gamma", "name", "alpha"] \[gamma, name, alpha] ->
+            [ ]
+            /\ --------
+            ( exp CCons [gamma, name, alpha] ⊢⊢ alpha )
+        S -> G.makeRule ["gamma", "alpha", "name", "beta"] \[gamma, alpha, name, beta] ->
+            [ gamma ⊢⊢ alpha ]
+            /\ --------
+            ( exp CCons [gamma, name, beta] ⊢⊢ alpha )
+        -- Miscellaneous
+        Newline -> G.makeRule ["s"] \[s] ->
+            [s]
+            /\ --------
+            s
 
 type DerivLabel = G.DerivLabel ExprLabel RuleLabel
 
@@ -143,115 +162,3 @@ infix 8 judgeTerm as ⊢
 
 judgeVar gamma alpha = exp SVar [gamma, alpha]
 infix 8 judgeVar as ⊢⊢
-
---data Rule l r = Rule (Set MetaVar) (Array (MetaExpr l)) (MetaExpr l)
-rules :: Array Rule
-rules = [
-    --------------------------- Terms ----------------------------------------------
-    -- app
-    let g = freshMetaVar' unit in
-    let a = freshMetaVar' unit in
-    let b = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [g, a, b])
-        [exp STerm [var g, exp TArrow [var a, var b]],   exp STerm [var g, var a]]
-        (exp STerm [var g, var b])
-    ,
-    -- lambda
-    let g = freshMetaVar' unit in
-    let a = freshMetaVar' unit in
-    let b = freshMetaVar' unit in
-    let n = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [g, a, b])
-        [
-            exp SType [var a], -- the type annotation (comment this line if we don't want annotations on lambdas)
-            exp STerm [exp CCons [var g, var n, var a], var b] -- the body
-        ]
-        (exp STerm [var g, exp TArrow [var a, var b]])
-    ,
-    -- var
-    let g = freshMetaVar' unit in
-    let t = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [g, t])
-        [exp SVar [var g, var t]]
-        (exp STerm [var g, var t])
-    ,
-    -- let
-    let g = freshMetaVar' unit in
-    let defTy = freshMetaVar' unit in
-    let bodyTy = freshMetaVar' unit in
-    let n = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [g, defTy, bodyTy])
-        [
-            exp SType [var defTy], -- type
-            exp STerm [exp CCons [var g, var n, var defTy], var defTy], -- definition
-            exp STerm [exp CCons [var g, var n, var defTy], var bodyTy]] --body
-        (exp STerm [var g, var bodyTy])
-
-    ,
-    -- if
-    let a = freshMetaVar' unit in
-    let g = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [a])
-        []
-        -- Bool -> a -> a -> a
-        (exp STerm [var g, exp TArrow [exp TBool [], exp TArrow [var a, exp TArrow [var a, var a]]]])
-
-
-    --------------------------- Types ----------------------------------------------
-    ,
-    -- base
-    G.Rule (Set.fromFoldable [])
-        []
-        (exp SType [exp TBase []])
-    ,
-    -- arrow
-    let a = freshMetaVar' unit in
-    let b = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [])
-        [exp SType [var a], exp SType [var b]]
-        (exp SType [exp TArrow [var a, var b]])
-
-    --------------------------- Debruin Indices ------------------------------------
-    ,
-    -- zero
-    let g = freshMetaVar' unit in
-    let t = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [g, t])
-        []
-        (exp SVar [exp CCons [var g,{-TODO: name-} var t], var t])
-    ,
-    -- suc
-    let g = freshMetaVar' unit in
-    let a = freshMetaVar' unit in
-    let b = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [g, a, b])
-        [exp SVar [var g, var a]]
-        (exp SVar [exp CCons [var g, {-TODO-} var b], var a])
-
-    --------------------------- Other ----------------------------------------------
-    ,
-    -- hole
-    let s = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [s])
-        []
-        (var s)
-    ,
-    -- newline
-    let s = freshMetaVar' unit in
-    G.Rule (Set.fromFoldable [s])
-        [var s]
-        (var s)
-
-{-
-
-Var G A
----------------------- suc
-Var (G , T) A
-
------------------------ zero
-Var (G , T) T
-
--}
-
-]
-
