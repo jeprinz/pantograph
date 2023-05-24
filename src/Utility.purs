b6c35c83c06d0bb88907661637a7da5631386ba1
Henry Blanchette <henry@monkeyface.com>
2023-05-24 19:36:50 +0800

    stripSuffix

--- src/Utility.purs
+++ src/Utility.purs
@@ -1,8 +1,12 @@
 module Utility where
 
-import Prelude
 import Data.Tuple.Nested
+import Prelude
+
 import Bug (bug)
+import Data.List (List(..), (:))
+import Data.List as List
+import Data.Maybe (Maybe(..))
 
 map2 f = map (map f)
 infixl 4 map2 as <$$>
@@ -10,3 +14,11 @@ infixl 4 map2 as <$$>
 map3 f = map (map (map f))
 infixl 4 map3 as <$$$>
 
+stripSuffix :: forall a. Eq a => List.Pattern a -> List a -> Maybe (List a)
+stripSuffix (List.Pattern Nil) xs = Just xs
+stripSuffix (List.Pattern suf) xs0 = go Nil xs0
+  where
+  go _ Nil = Nothing
+  go ys (x : xs)
+    | suf == xs = Just (List.reverse (x : ys))
+    | otherwise = go (x : ys) xs
