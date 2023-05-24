1a064eabb713d9f42d3a22bbcc64d6e207cc0b65
Henry Blanchette <henry@monkeyface.com>
2023-05-24 16:50:29 +0800

    cleaned up imports

--- src/Language/Pantograph/ULC/Rendering.purs
+++ src/Language/Pantograph/ULC/Rendering.purs
@@ -4,7 +4,6 @@ import Prelude
 
 import Bug.Assertion (assert)
 import Data.Array as Array
-import Data.Expr ((%))
 import Data.Expr as Expr
 import Data.Tuple (fst)
 import Data.Tuple.Nested (type (/\), (/\))
@@ -12,14 +11,10 @@ import Effect.Aff (Aff)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.Hooks as HK
-import Halogen.Utilities (classNames)
-import Hole as Hole
-import Language.Pantograph.Generic.Grammar ((|-))
 import Language.Pantograph.Generic.Grammar as Grammar
 import Language.Pantograph.Generic.Rendering (defaultEditsAtHoleyDerivZipper)
 import Language.Pantograph.Generic.Rendering as Rendering
-import Language.Pantograph.ULC.Grammar (DerivExpr, DerivZipper, ExprLabel, RuleLabel(..), MetaExpr)
-import Text.Pretty (pretty)
+import Language.Pantograph.ULC.Grammar (DerivExpr, ExprLabel, MetaExpr, RuleLabel(..))
 
 type Query = Rendering.Query ExprLabel RuleLabel
 type Output = Rendering.Output ExprLabel RuleLabel
@@ -76,45 +71,3 @@ type Edit = Grammar.Edit ExprLabel RuleLabel
 type HoleyDerivZipper = Rendering.HoleyDerivZipper ExprLabel RuleLabel
 
 editsAtHoleyDerivZipper = defaultEditsAtHoleyDerivZipper
-
--- -- !TODO eventualy this should not even require `DerivZipper` as an arg (??)
--- getEdits :: HoleyDerivZipper -> Array Edit
--- -- getEdits _ = Hole.hole "!TODO derive default edits from language"
--- getEdits _ = Grammar.defaultEdits
-
--- getEdits = assertInput_ (\(Expr.Zipper dz) -> wellformedExpr "getEdits" dz.expr) \(Expr.Zipper dz) -> do
---   let
---     -- digEdit sort = 
---     --   { label: "dig", preview: "?"
---     --   , action: SetDerivZipperAction $ defer \_ -> Just $ Expr.Zipper dz {expr = holeDE holeInteriorDE sort} }
-    
---     enTooth label preview tooth =
---       { label, preview
---       , action: SetDerivZipperAction $ defer \_ -> Just $ Expr.Zipper dz {path = Expr.stepPath tooth dz.path} }
-
---     inTooth label preview path' tooth innerSort =
---       { label, preview
---       -- , action: SetDerivZipperAction $ defer \_ -> Just $ Expr.Zipper dz {path = Expr.stepPath tooth path', expr = holeDE holeInteriorDE innerSort}
---       , action: SetDerivZipperAction $ defer \_ -> Just $ Expr.Zipper dz {path = Expr.stepPath tooth path', expr = ?a}
---       }
-    
---     inFill label preview path' expr =
---       { label, preview
---       , action: SetDerivZipperAction $ defer \_ -> Just $ Expr.Zipper dz {path = path', expr = expr}
---       }
-    
---     enSuc = enTooth "suc" "S⌶" (Suc |- ?varSortME %< mempty)
---     enLam = enTooth "lam" "lam ? => ⌶" $ Lam |- ?termSortME %< ZipList.singletonLeft (?holeDE holeInteriorDE varSortME)
---     enApl = enTooth "apl" "⌶ ?" $ App |- termSortME %< ZipList.singletonRight (holeDE holeInteriorDE varSortME)
---     enArg = enTooth "arg" "? ⌶" $ App |- termSortME %< ZipList.singletonLeft (holeDE holeInteriorDE varSortME)
---     inZero path' = inFill "zero" "Z" path' zeroDE
---     inRef path' = inTooth "ref" "ref ⌶" path' (Ref |- termSortME %< mempty) varSortME
---   case dz of
---     -- var
---     {expr: _ |- (Expr.Meta (Right VarSort) % _) % _} -> [digEdit varSortME, enSuc]
---     -- term
---     {expr: _ |- (Expr.Meta (Right TermSort) % []) % _} -> [digEdit termSortME, enLam, enApl, enArg]
---     -- hole interior
---     {path: Expr.Path ((Hole |- (Expr.Meta (Right VarSort) % _) %< _) : ths), expr: HoleInterior |- (Expr.Meta (Right HoleInteriorSort) % _) % _} -> [inZero (Expr.Path ths)]
---     {path: Expr.Path ((Hole |- (Expr.Meta (Right TermSort) % _) %< _) : ths), expr: HoleInterior |- (Expr.Meta (Right HoleInteriorSort) % _) % _} -> [inRef (Expr.Path ths)]
-
