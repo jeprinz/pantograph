3213279af5a889b493e995f522623a3fca8f8a3a
Jacob Prinz <jacobeliasprinz@gmail.com>
2023-09-24 11:56:29 -0400

    Progress on language specific file

--- src/Language/Pantograph/Specific/October14.purs
+++ src/Language/Pantograph/Specific/October14.purs
@@ -55,8 +55,6 @@ It should be STLC with no partial applications, some built in types, and thats a
 
 We could maybe consider having a top level of the program and a main function.
 We should think about how it will be run.
-
-For now this is just the type system, but later we will copy the rest from STLC.
 -}
 
 --------------------------------------------------------------------------------
@@ -82,7 +80,7 @@ instance Pretty DataType where
   pretty = show
 
 data PreSortLabel
-  -- Big ones
+  -- Judgements
   = VarSort {-Ctx-} {-String-} {-Type-} {-Local or NonLocal-}
   | TermSort {-Ctx-} {-Type-}
   | NeutralSort {-Ctx-} {-Type-}
@@ -96,6 +94,72 @@ data PreSortLabel
   -- Locality
   | Local
   | NonLocal
+
+derive instance Generic PreSortLabel _
+instance Show PreSortLabel where show x = genericShow x
+instance Eq PreSortLabel where eq x = genericEq x
+instance Ord PreSortLabel where compare x y = genericCompare x y
+
+instance Pretty PreSortLabel where
+  pretty = show
+
+instance IsExprLabel PreSortLabel where
+  prettyExprF'_unsafe (VarSort /\ [gamma, x, ty, locality]) = "Var" <+> parens gamma <+> x <+> ty <+> "(" <> show locality <> ")"
+  prettyExprF'_unsafe (TermSort /\ [gamma, ty]) = "Term" <+> parens gamma <+> ty
+  prettyExprF'_unsafe (NeutralSort /\ [gamma, ty]) = "Neutral" <+> parens gamma <+> ty
+  prettyExprF'_unsafe (TypeSort /\ [t]) = "Type" <+> parens t
+  prettyExprF'_unsafe (CtxConsSort /\ [x, ty, "∅"]) = x <> ":" <> ty
+  prettyExprF'_unsafe (CtxConsSort /\ [x, ty, gamma]) = x <> ":" <> ty <> ", " <> gamma
+  prettyExprF'_unsafe (CtxNilSort /\ []) = "∅"
+  prettyExprF'_unsafe (Local /\ []) = "Local"
+  prettyExprF'_unsafe (NonLocal /\ []) = "NonLocal"
+  prettyExprF'_unsafe (DataType ty /\ []) = show ty
+  prettyExprF'_unsafe (Arrow  /\ [a, b]) = a <> " -> " <> b
+
+
+  expectedKidsCount VarSort = 4
+  expectedKidsCount TermSort = 2
+  expectedKidsCount NeutralSort = 2
+  expectedKidsCount TypeSort = 1
+  expectedKidsCount CtxConsSort = 3
+  expectedKidsCount CtxNilSort = 0
+  expectedKidsCount Local = 0
+  expectedKidsCount NonLocal = 0
+  expectedKidsCount (DataType _) = 0
+  expectedKidsCount Arrow = 2
+
+--------------------------------------------------------------------------------
+-- Shorter Aliases
+--------------------------------------------------------------------------------
+
+-- Expr
+type Expr = Expr.Expr PreSortLabel
+type MetaExpr = Expr.MetaExpr PreSortLabel
+type Zipper = Expr.Zipper PreSortLabel
+type Tooth = Expr.Tooth PreSortLabel
+type Sort = Grammar.Sort PreSortLabel
+
+-- Grammar
+type DerivTerm = Grammar.DerivTerm PreSortLabel RuleLabel
+type DerivLabel = Grammar.DerivLabel PreSortLabel RuleLabel
+type DerivPath dir = Grammar.DerivPath dir PreSortLabel RuleLabel
+type DerivZipper = Grammar.DerivZipper PreSortLabel RuleLabel
+type DerivZipperp = Grammar.DerivZipperp PreSortLabel RuleLabel
+type SSTerm = Smallstep.SSTerm PreSortLabel RuleLabel
+type LanguageChanges = Grammar.LanguageChanges PreSortLabel RuleLabel
+type SortChange = Grammar.SortChange PreSortLabel
+type ChangeRule = Grammar.ChangeRule PreSortLabel
+
+-- Rendering
+type Query = Rendering.Query
+type Output = Rendering.Output PreSortLabel RuleLabel
+type HoleyDerivZipper = Rendering.HoleyDerivZipper PreSortLabel RuleLabel
+
+type Edit = Edit.Edit PreSortLabel RuleLabel
+
+--------------------------------------------------------------------------------
+-- RuleLabel
+--------------------------------------------------------------------------------
 
 -- | Naming convention: <title>_<output sort>
 data RuleLabel
@@ -111,7 +175,7 @@ data RuleLabel
   | TypeHole
   | DataTypeRule DataType
   | ArrowRule
-  | FormatRule Format
+  | Newline -- TODO: is this really an acceptable way for newlines to work? Its broken for applications, isn't it?
 
 derive instance Generic RuleLabel _
 derive instance Eq RuleLabel
@@ -127,27 +191,53 @@ instance Bounded RuleLabel where
 instance Pretty RuleLabel where
   pretty = show
 
-data Format
-  = Newline
-  | Comment
-
-derive instance Generic Format _
-derive instance Eq Format
-derive instance Ord Format
-instance Show Format where show x = genericShow x
-instance Enum Format where
-  pred x = genericPred x
-  succ x = genericSucc x
-instance Bounded Format where
-  bottom = genericBottom
-  top = genericTop
-
 --------------------------------------------------------------------------------
 -- Language
 --------------------------------------------------------------------------------
 
 type Language = Grammar.Language PreSortLabel RuleLabel
 type Rule = Grammar.Rule PreSortLabel
+
+sortToType :: Sort -> DerivTerm
+sortToType (Expr.Expr (Expr.Meta (Right (Grammar.InjectSortLabel (DataType dt)))) []) =
+    Grammar.makeLabel (DataTypeRule dt) [] % []
+sortToType (Expr.Expr (Expr.Meta (Right (Grammar.InjectSortLabel Arrow))) [a, b]) =
+    Grammar.makeLabel ArrowRule ["a" /\ a, "b" /\ b] % [sortToType a, sortToType b]
+sortToType ty =
+    Grammar.makeLabel TypeHole ["type" /\ ty] % []
+--    Smallstep.termToSSTerm $ assertI $ just "sortToType: invalid sort"
+--    $ Grammar.defaultDerivTerm (TypeSort %|-* [s])
+
+instance Grammar.IsRuleLabel PreSortLabel RuleLabel where
+  prettyExprF'_unsafe_RuleLabel (Zero /\ []) = pretty Zero
+  prettyExprF'_unsafe_RuleLabel (Suc /\ [x]) = pretty Suc <> x
+  prettyExprF'_unsafe_RuleLabel (Lam /\ [x, ty, b]) = P.parens $ "λ" <+> x <+> ":" <+> ty <+> "↦" <+> b
+  prettyExprF'_unsafe_RuleLabel (Let /\ [x, ty, a, b]) = P.parens $ "let" <+> x <+> ":" <+> ty <+> "=" <+> a <+> b
+  prettyExprF'_unsafe_RuleLabel (ArrowRule /\ [a, b]) = P.parens $ a <+> "->" <+> b
+  prettyExprF'_unsafe_RuleLabel (DataTypeRule dataType /\ []) = pretty dataType
+  prettyExprF'_unsafe_RuleLabel (App /\ [f, a]) = P.parens $ f <+> a
+  prettyExprF'_unsafe_RuleLabel (Ref /\ [x]) = "@" <> x
+  prettyExprF'_unsafe_RuleLabel (TermHole /\ []) = "?"
+  prettyExprF'_unsafe_RuleLabel (TypeHole /\ []) = "?<type>"
+  prettyExprF'_unsafe_RuleLabel (Newline /\ [a]) = "<newline> " <> a
+  prettyExprF'_unsafe_RuleLabel (FreeVar /\ []) = "free"
+  prettyExprF'_unsafe_RuleLabel (FunctionCall /\ [neu]) = "call" <+> P.parens neu
+  prettyExprF'_unsafe_RuleLabel other = bug ("[prettyExprF'...] the input was: " <> show other)
+
+  language = language
+
+  isHoleRuleTotalMap = TotalMap.makeTotalMap case _ of
+    TermHole -> true
+    TypeHole -> true
+    _ -> false
+
+  defaultDerivTerm' (Expr.Meta (Right (Grammar.InjectSortLabel TermSort)) % [gamma, ty]) = pure (Grammar.makeLabel TermHole ["gamma" /\ gamma, "type" /\ ty] % [])
+  defaultDerivTerm' (Expr.Meta (Right (Grammar.InjectSortLabel VarSort)) % [_gamma, _x, _ty, _locality]) = empty
+  defaultDerivTerm' (Expr.Meta (Right (Grammar.InjectSortLabel TypeSort)) % [ty]) =
+    pure $ sortToType ty
+--    pure (Grammar.makeLabel TypeHole ["type" /\ ty] % [])
+  defaultDerivTerm' (Expr.Meta (Right Grammar.NameSortLabel) % [_]) = pure $ Grammar.DerivString "" % []
+  defaultDerivTerm' sort = bug $ "[defaultDerivTerm] no match: " <> pretty sort
 
 language :: Language
 language = TotalMap.makeTotalMap case _ of
@@ -195,14 +285,8 @@ language = TotalMap.makeTotalMap case _ of
     /\ --------
     ( TermSort %|-* [gamma, ty] )
 
-  FormatRule Newline -> Grammar.makeRule ["s"] \[s] ->
+  Newline -> Grammar.makeRule ["s"] \[s] ->
     [ s ]
-    /\ --------
-    ( s )
-
-  FormatRule Comment -> Grammar.makeRule ["comment", "gamma"] \[comment, s] ->
-    [ Grammar.NameSortLabel %* [comment]
-    , s ]
     /\ --------
     ( s )
 
@@ -229,3 +313,75 @@ language = TotalMap.makeTotalMap case _ of
     /\ --------
     ( TypeSort %|-* [Arrow %|-* [a, b]] )
 
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+languageChanges :: LanguageChanges
+languageChanges = Grammar.defaultLanguageChanges language # TotalMap.mapWithKey case _ of
+  _ -> identity
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- Rendering
+--------------------------------------------------------------------------------
+
+arrangeDerivTermSubs :: Unit -> Rendering.ArrangeDerivTermSubs PreSortLabel RuleLabel
+arrangeDerivTermSubs _ {renCtx, rule, sort} = case rule /\ sort of
+  _ /\ (Expr.Meta (Right (Grammar.InjectSortLabel VarSort)) %
+    [ _gamma
+    , Expr.Meta (Right (Grammar.StringSortLabel str)) % []
+    , _ty
+    , locality ]) ->
+    -- TODO: use locality in rendering?
+    let postfix = if locality == sor Local % [] then "" else "!" in
+    [pure [nameElem (str <> postfix)]]
+  -- term
+  Ref /\ _ ->
+    [pure [refElem], Left (renCtx /\ 0)]
+  Lam /\ _ ->
+    let renCtx' = Rendering.incremementIndentationLevel renCtx in
+    [pure [Rendering.lparenElem, lambdaElem], Left (renCtx /\ 0), pure [colonElem], Left (renCtx /\ 1), pure [mapstoElem], Left (renCtx' /\ 2), pure [Rendering.rparenElem]]
+  Let /\ _ ->
+    let renCtx' = Rendering.incremementIndentationLevel renCtx in
+    [pure [letElem], Left (renCtx /\ 0), pure [colonElem], Left (renCtx /\ 1), pure [equalsElem], Left (renCtx' /\ 2), pure [inElem], Left (renCtx' /\ 3)]
+  App /\ _ ->
+    let renCtx' = Rendering.incremementIndentationLevel renCtx in
+    [pure [Rendering.lparenElem], Left (renCtx' /\ 0), pure [Rendering.spaceElem], Left (renCtx' /\ 1), pure [Rendering.rparenElem]]
+  FunctionCall /\ _ ->
+    [Left (renCtx /\ 0)]
+  -- types
+  DataTypeRule dataType /\ _ ->
+    [pure [dataTypeElem (pretty dataType)]]
+  ArrowRule /\ _ ->
+    let renCtx' = Rendering.incremementIndentationLevel renCtx in
+    [Left (renCtx' /\ 0), pure [arrowElem], Left (renCtx' /\ 1)]
+  -- format
+  Newline /\ _ ->
+    Array.concat
+      [ if renCtx.isInlined then [] else
+        [pure $ [Rendering.spaceElem] <> [Rendering.newlineElem] <> Array.replicate renCtx.indentationLevel Rendering.indentElem]
+      , [Left (renCtx /\ 0)] ]
+  -- hole
+  TermHole /\ _ -> bug $
+    "[STLC.Grammar.arrangeDerivTermSubs] `TermHole` should be handled generically instead of here"
+  _ -> bug $
+    "[STLC.Grammar.arrangeDerivTermSubs] no match" <> "\n" <>
+    "  - rule = " <> pretty rule <> "\n" <>
+    "  - sort = " <> show sort
+
+lambdaElem = Rendering.makePuncElem "lambda" "λ"
+mapstoElem = Rendering.makePuncElem "mapsto" "↦"
+refElem = Rendering.makePuncElem "ref" "#"
+zeroVarElem = Rendering.makePuncElem "zeroVar" "Z"
+sucVarElem = Rendering.makePuncElem "sucVar" "S"
+-- Am I doing this right?
+colonElem = Rendering.makePuncElem "colon" ":"
+equalsElem = Rendering.makePuncElem "equals" "="
+inElem = Rendering.makePuncElem "inLet" "in"
+letElem = Rendering.makePuncElem "let" "let"
+arrowElem = Rendering.makePuncElem "arrow" "→"
+
+nameElem str = HH.span [classNames ["name"]] [HH.text str]
+dataTypeElem str = HH.span [classNames ["datatype"]] [HH.text str]
