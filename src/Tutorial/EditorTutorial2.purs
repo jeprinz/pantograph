8997a2fbd2bb437c8539455c3e917c1a1bfb4a38
Henry Blanchette <henry@monkeyface.com>
2023-11-30 17:48:34 +0800

    red squiggly underline grey arg

--- src/Tutorial/EditorTutorial2.purs
+++ src/Tutorial/EditorTutorial2.purs
@@ -66,7 +66,7 @@ makePantographTutorial spec lessons interpereter =
       editorComponent = Editor.editorComponent unit
       convertPaths markedPaths = markedPaths <#> \path -> (Base.HoleyDerivPath path false)
       initialState _ = {
-        activeLesson : 0
+        activeLesson : 12--0
         , lessonsSolved : Array.replicate (Array.length lessons) false
         , output : ""
         }
