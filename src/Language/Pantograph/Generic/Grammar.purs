c6e788c70c5753acd4f57e57dbf3efe0815eda9d
Henry Blanchette <henry@monkeyface.com>
2026-04-06 11:40:27 -0400

    special case of defaultDerivTerm to get the string in a DerivLabel (DataString s)

--- src/Language/Pantograph/Generic/Grammar.purs
+++ src/Language/Pantograph/Generic/Grammar.purs
@@ -3,53 +3,42 @@ module Language.Pantograph.Generic.Grammar where
 import Prelude
 
 import Bug (bug)
-import Bug.Assertion (Assertion, assert, assertInput_, assertInterface_, equal, just, makeAssertionBoolean)
+import Bug.Assertion (Assertion, assert, assertInput_, equal, makeAssertionBoolean)
+import Control.Monad.Except.Trans (ExceptT)
+import Control.Monad.State (State)
 import Control.Plus (empty)
-import Data.Array as Array
-import Data.Bifunctor (lmap)
-import Data.Either as Either
-import Data.Either (Either(..))
-import Data.Enum (class Enum)
-import Data.Expr (class IsExprLabel, class ReflectPathDir, MetaVar(..), expectedKidsCount, freshMetaVar, prettyExprF'_unsafe, reflectPathDir, (%), (%*))
-import Data.Expr as Expr
-import Data.Generic.Rep (class Generic)
-import Data.List (List(..), (:))
-import Data.List as List
-import Data.List.Rev as RevList
-import Data.List.Zip as ZipList
-import Data.Map (Map)
-import Data.Map as Map
-import Data.Maybe (Maybe(..))
-import Data.Set (Set)
-import Data.Set as Set
-import Data.Show.Generic (genericShow)
-import Data.TotalMap (TotalMap)
-import Data.TotalMap as TotalMap
-import Data.Traversable (class Foldable, class Traversable)
-import Data.Traversable (sequence)
-import Data.Tuple (fst, snd)
-import Data.Tuple.Nested (type (/\), (/\))
-import Data.Variant (case_, on)
-import Debug (trace, traceM)
-import Hole (hole)
-import Hole as Hole
-import Language.Pantograph.Generic.ChangeAlgebra (diff)
-import Language.Pantograph.Generic.Unification (class Freshenable, Sub, freshen', unifyLists, composeSub, composeSubs, unify, unifyFImpl, flattenSub, runUnifyMonad)
-import Partial.Unsafe (unsafePartial)
-import Text.Pretty (class Pretty, bullets, pretty)
-import Type.Direction (_down, _up)
-import Type.Direction as Dir
-import Util as Util
-import Data.Foldable as Foldable
 import Data.Argonaut (stringify)
 import Data.Argonaut.Decode.Class (class DecodeJson, decodeJson)
 import Data.Argonaut.Decode.Generic (genericDecodeJson)
 import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
 import Data.Argonaut.Encode.Generic (genericEncodeJson)
 import Data.Argonaut.Parser (jsonParser)
-import Control.Monad.Except.Trans (ExceptT, runExceptT)
-import Control.Monad.State (State)
-import Control.Monad.State as State
+import Data.Array as Array
+import Data.Bifunctor (lmap)
+import Data.Either (Either(..))
+import Data.Enum (class Enum)
+import Data.Expr (class IsExprLabel, Meta(..), MetaVar(..), expectedKidsCount, freshMetaVar, prettyExprF'_unsafe, (%), (%*))
+import Data.Expr as Expr
+import Data.Generic.Rep (class Generic)
+import Data.List (List(..), (:))
+import Data.List as List
+import Data.List.Rev as RevList
+import Data.List.Zip as ZipList
+import Data.Map as Map
+import Data.Maybe (Maybe(..))
+import Data.Set as Set
+import Data.Show.Generic (genericShow)
+import Data.TotalMap (TotalMap)
+import Data.TotalMap as TotalMap
+import Data.Traversable (class Foldable, class Traversable, sequence)
+import Data.Tuple (fst, snd)
+import Data.Tuple.Nested (type (/\), (/\))
+import Language.Pantograph.Generic.ChangeAlgebra (diff)
+import Language.Pantograph.Generic.Unification (class Freshenable, Sub, composeSub, freshen', runUnifyMonad, unify, unifyFImpl, unifyLists)
+import Partial.Unsafe (unsafePartial)
+import Text.Pretty (class Pretty, bullets, pretty)
+import Type.Direction as Dir
+import Util as Util
 
 --------------------------------------------------------------------------------
 -- RuleLabel
@@ -73,8 +62,9 @@ class (Expr.IsExprLabel l, Eq r, Enum r, Bounded r, Show r, Pretty r,
 -- a term with the sort you gave it. On the other hand, if you give it a sort that is allowed to appear in the
 -- program, then it will always return a term with the given sort. So we should think of what the exact criteria is here.
 defaultDerivTerm :: forall l r. IsRuleLabel l r => Sort l -> Maybe (DerivTerm l r)
-defaultDerivTerm (Expr.MInj (TypeOfLabel ty) % [_]) =
+defaultDerivTerm (Expr.MInj (TypeOfLabel ty) % [x]) =
     case ty of
+        SortString | MInj (DataLabel (DataString s)) % _ <- x -> pure $ DerivLiteral (DataString s) % []
         SortString -> pure $ DerivLiteral (DataString "") % []
         SortInt -> pure $ DerivLiteral (DataInt 0) % []
 defaultDerivTerm sort = assert (Expr.wellformedExpr "defaultDerivTerm" sort) \_ ->
